9f5322be57425c22fc5638789574513d735f84b0
Alex Thompson <ahthomps1@gmail.com>
2021-06-01 15:40:39 -0600

    Create basic structure of NewsView

--- src/NewsView/NewsView.js
+++ src/NewsView/NewsView.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Article from '../Article/Article';
 import './NewsView.css';
+import Story from '../Article/Article';
 
-const NewsView = () => {
+const NewsView = ({ stories }) => {
+
+  // const storyCards = stories.map(story => {
+  //   return (
+  //     <Article
+  //     />
+  //   )
+  // })
 
   return (
     <h1>Articles</h1>
