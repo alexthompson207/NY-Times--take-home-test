fd43962b332fe3b5c7e7f44d1c5d20227817fe2f
Alex Thompson <ahthomps1@gmail.com>
2021-06-01 15:52:22 -0600

    Create a file for apiCalls, and connect App component to fetch all top stories from API on page render

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import NewsView from '../NewsView/NewsView';
+import { getAllStories } from '../apiCalls';
 
 const App = () => {
+
+  const [stories, setStories] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getAllStories()
+      .then(response => {
+        if (response.status === 'OK') {
+          setStories(response.results)
+        } else {
+          setError('fetch error')
+        }
+      })
+      .catch(err => setError('fetch error'))
+
+  }, []);
+
   return (
     <div className="App">
       hi
