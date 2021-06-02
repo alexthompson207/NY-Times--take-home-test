c357e8acbb7dfdfb223d83f4eff1ebccb7393500
Alex Thompson <ahthomps1@gmail.com>
2021-06-02 08:42:47 -0600

    Add publishedDate and section to article details view

--- src/ArticleDetail/ArticleDetail.js
+++ src/ArticleDetail/ArticleDetail.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const ArticleDetail = ({ currentStory }) => {
 
-  const { title, photo, photoAlt, overview, author, link } = currentStory;
+  const { title, photo, photoAlt, overview, author, link, section, publishedDate } = currentStory;
 
   return (
     <section className='article-details-view'>
@@ -13,6 +13,8 @@ const ArticleDetail = ({ currentStory }) => {
       <img className='details-img' src={photo} alt={photoAlt}></img>
       <h2 className='details-overview'>{overview}</h2>
       <p className='details-author'>{author}</p>
+      <p className='details-section'>{section}</p>
+      <p className='details-date'>{publishedDate}</p>
       <a href={link} className='details-link' target='_blank' rel='noreferrer'>View the Article</a>
     </section>
   )
