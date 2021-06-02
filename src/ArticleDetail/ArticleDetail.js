a3a0fafd2d4539d2c6afb682a84235bcbf4dd78d
Alex Thompson <ahthomps1@gmail.com>
2021-06-02 11:56:44 -0600

    Add basic styling to articleDetail component and add styling to back button to return to homepage

--- src/ArticleDetail/ArticleDetail.js
+++ src/ArticleDetail/ArticleDetail.js
@@ -7,16 +7,20 @@ const ArticleDetail = ({ currentStory }) => {
   const { title, photo, photoAlt, overview, author, link, section, publishedDate } = currentStory;
 
   return (
-    <section className='article-details-view'>
-      <Link to='/'>Back</Link>
-      <h1 className='details-title'>{title}</h1>
-      <img className='details-img' src={photo} alt={photoAlt}></img>
-      <h2 className='details-overview'>{overview}</h2>
-      <p className='details-author'>{author}</p>
-      <p className='details-section'>{section}</p>
-      <p className='details-date'>{publishedDate}</p>
-      <a href={link} className='details-link' target='_blank' rel='noreferrer'>View the Article</a>
-    </section>
+    <>
+      <Link className='back-btn' to='/'>Back</Link>
+      <section className='article-details-view'>
+        <h1 className='details-title'>{title}</h1>
+        <img className='details-img' src={photo} alt={photoAlt}></img>
+        <h2 className='details-overview'>{overview}</h2>
+        <p className='details-author'>{author}</p>
+        <div className='details-detail'>
+          <p className='details-section'>{section.toUpperCase()}</p>
+          <p className='details-date'>{publishedDate}</p>
+        </div>
+        <a href={link} className='details-link' target='_blank' rel='noreferrer'>View the full article</a>
+      </section>
+    </>
   )
 }
 
