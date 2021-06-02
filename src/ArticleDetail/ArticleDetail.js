252a3d7886a5d33fbe15d103a27fd2310b158b7a
Alex Thompson <ahthomps1@gmail.com>
2021-06-01 21:15:30 -0600

    Pass down currentStory as props to ArticleDetails componet

--- src/ArticleDetail/ArticleDetail.js
+++ src/ArticleDetail/ArticleDetail.js
@@ -1,7 +1,9 @@
 import './ArticleDetail.css';
 import React from 'react';
 
-const ArticleDetail = () => {
+const ArticleDetail = ({ currentStory }) => {
+
+  const { title, photo, photoAlt, overview, author, link } = currentStory;
 
   return (
     <section className='article-details-view'>
@@ -9,7 +11,7 @@ const ArticleDetail = () => {
       <img className='details-img' src={photo} alt={photoAlt}></img>
       <h2 className='details-overview'>{overview}</h2>
       <p className='details-author'>{author}</p>
-      <a href={link} className='details-link' target='_blank' rel='noreferrer'>View the Article on the NYT</a>
+      <a href={link} className='details-link' target='_blank' rel='noreferrer'>View the Article</a>
     </section>
   )
 }
